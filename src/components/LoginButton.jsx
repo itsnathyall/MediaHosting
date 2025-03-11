023d570b413300214dc84e84a1ea28824761960b
Nathan Caluwaerts <nathan.caluwaerts@gmail.com>
2025-03-11 16:23:12 +0100

    added glitch effect button

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,5 +1,7 @@
+import '../button.css'
+
 export default function LoginButton(){
     return(    
-        <button className='px-10 py-2 text-2xl rounded-md text-gray-200 hover:text-gray-400 bg-red-900 hover:bg-red-950'>Login</button>
-    )
+        <button className="glitch">Login</button>
+ )
 }
