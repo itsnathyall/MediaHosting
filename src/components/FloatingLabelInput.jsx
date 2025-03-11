0aa97e4ee9162db7f5c649b5f22996c30d9d96fe
Nathan Caluwaerts <nathan.caluwaerts@gmail.com>
2025-03-11 15:15:20 +0100

    added components folder

--- src/components/FloatingLabelInput.jsx
+++ src/components/FloatingLabelInput.jsx
@@ -11,12 +11,12 @@ export default function FloatingLabelInput  ({ label, type = "text", id, name })
         name={name}
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        className="peer w-full border border-gray-300 rounded-md p-3 pt-5 text-gray-900"
+        className="peer w-full border border-gray-300 rounded-md p-3 pt-5 text-gray-900 focus:border-red"
         placeholder={label}/>
       <label
         htmlFor={id}
-        className={`absolute left-3 top-4 text-gray-400 transition-all duration-200 
-          peer-placeholder-shown:top-4 peer-placeholder-shown:text-gray-500 
+        className={`absolute left-3 top-4 text-gray-300 transition-all duration-200 
+          peer-placeholder-shown:top-4 peer-placeholder-shown:text-gray-300 
           peer-focus:top-1 peer-focus:text-sm`}>
         {label}
       </label>
