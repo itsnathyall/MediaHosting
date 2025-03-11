c6ee451dca82721aff6adcdc814ae7137e84c798
Nathan Caluwaerts <nathan.caluwaerts@gmail.com>
2025-03-11 13:46:09 +0100

    added tailwind react and colors

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+
+  return (
+    <div class="h-screen w-screen bg-[linear-gradient(-45deg,#000000_0%,#200000_45%,#200000_55%,#000000_100%)] 
+    flex items-center justify-center font-mono">
+      <div className='flex shadow-2xl'>
+        <div className='flex flex-col items-center justify-center text-center p-20 gap-8
+        bg-white rounded-2xl'>
+
+          <h1 className='text-5xl font-bold'>Welcome back</h1>
+
+        </div>
+      </div>
+    </div>
+      )
+}
+
+export default App
